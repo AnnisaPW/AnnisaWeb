1c73f1f787287dc6f9aea4e0d9977f8bd8572ce3
Annisa Putri <annisaputriwahyuni46@gmail.com>
2022-04-19 14:19:00 +0700

    fix all imports

--- lib/ui_screens/b_home_snake/_index.dart
+++ lib/ui_screens/b_home_snake/_index.dart
@@ -1,4 +1,4 @@
-import 'package:annisa_web/common/utils/_index.dart';
+import '../../common/utils/_index.dart';
 import 'package:flutter/material.dart';
 import 'package:states_rebuilder/states_rebuilder.dart';
 
